d0bd989b6227c492307aa42e47104dbb7c122efa
NarshNixWebworks <harshnandi0207@gmail.com>
2026-03-06 19:00:11 +0530

    made the db model and backend routes

---
+++ backend/routes/authRoute.js
@@ -0,0 +1,41 @@
+import express from "express";
+import { body } from "express-validator";
+import {
+  register,
+  login,
+  getProfile,
+  updateProfile,
+  changePassword,
+} from "../controller/authController.js";
+
+import protect from "../middleware/auth.js";
+
+const router = express.Router();
+
+const registerValidation = [
+  body("username")
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage("Username must be atleast 3 characters"),
+  body("email")
+    .isEmail()
+    .normalizeEmail()
+    .withMessage("Please provide a valid email"),
+];
+
+const loginValidation = [
+  body("email")
+    .isEmail()
+    .normalizeEmail()
+    .withMessage("Please provide a valid email"),
+  body("password").notEmpty().withMessage("Password is required"),
+];
+
+router.post("/register", registerValidation, register);
+router.post("/login", loginValidation, login);
+
+router.get("/profile", protect, getProfile);
+router.put("/profile", protect, updateProfile);
+router.post("/change-password", protect, changePassword);
+
+export default router;
